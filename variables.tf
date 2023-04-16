ba33ce04409afbe2854852a61cdfb2eb8ba2261e
Anirudh198123 <100208834+Anirudh198123@users.noreply.github.com>
2023-04-16 16:32:35 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "sg_name" {
+  type = string
+}
+variable "ingress_from_port" {
+  type = string
+}
+variable "ingress_to_port" {
+  type = string
+}
+variable "ingress_protocol" {
+  type = string
+}
+variable "egress_from_port" {
+  type = string
+}
+variable "egress_to_port" {
+  type = string
+}
+variable "egress_protocol" {
+  type = string
+}
