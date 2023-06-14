e4d9783ad6464db5ded48aeccfc093d2290259a9
Anirudh198123 <100208834+Anirudh198123@users.noreply.github.com>
2023-06-14 22:11:35 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -23,3 +23,27 @@ resource "aws_security_group" "Alltraffic" {
   }
 }
 
+===============================================================
+variable "ingress_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [8200, 8201, 8300, 9200]
+}
+
+resource "aws_security_group" "default_sg" {
+  name        = "dynamic-sg"
+  description = "Ingress for vault"
+
+  dynamic "ingress" {
+    for_each = var.ingress_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
+
